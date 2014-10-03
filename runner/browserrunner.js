b690589cfb3be0e309e7b4f5d7ebfe826d70ce94
Ian MacLeod <imac@google.com>
2014-10-03 15:18:04 -0700

    Support webRunner paths that include query params

--- runner/browserrunner.js
+++ runner/browserrunner.js
@@ -75,8 +75,8 @@ function BrowserRunner(emitter, local, def, options, doneCallback) {
 BrowserRunner.prototype.startTest = function startTest() {
   var host  = 'http://localhost:' + this.options._httpPort;
   var path  = '/' + this.options.component + '/' + this.options.webRunner;
-  var query = '?cli_browser_id=' + this.def.id;
-  this.browser.get(host + path + query, function(error) {
+  var extra = (path.indexOf('?') === -1 ? '?' : '&') + 'cli_browser_id=' + this.def.id;
+  this.browser.get(host + path + extra, function(error) {
     if (error) {
       this.done(error.data || error);
     } else {
