dd4330cd4f1007de48307362419c7374f4fb7fa1
Ian MacLeod <imac@google.com>
2014-11-13 15:32:51 -0800

    Document test events

--- runner/test.js
+++ runner/test.js
@@ -15,16 +15,65 @@ var CliReporter = require('./clireporter');
 var config      = require('./config');
 var steps       = require('./steps');
 
+/**
+ * Runs a suite of web component tests.
+ *
+ * The returned EventEmitter fires various events to allow you to track the
+ * progress of the tests:
+ *
+ * Lifecycle Events:
+ *
+ * `run-start`
+ *   WCT is ready to begin spinning up browsers.
+ *
+ * `browser-init` {browser} {stats}
+ *   WCT is ready to begin spinning up browsers.
+ *
+ * `browser-start` {browser} {metadata} {stats}
+ *   The browser has begun running tests. May fire multiple times (i.e. when
+ *   manually refreshing the tests).
+ *
+ * `sub-suite-start` {browser} {sharedState} {stats}
+ *   A suite file has begun running.
+ *
+ * `test-start` {browser} {test} {stats}
+ *   A test has begun.
+ *
+ * `test-end` {browser} {test} {stats}
+ *  A test has ended.
+ *
+ * `sub-suite-end` {browser} {sharedState} {stats}
+ *   A suite file has finished running all of its tests.
+ *
+ * `browser-end` {browser} {error} {stats}
+ *   The browser has completed, and it shutting down.
+ *
+ * `run-end` {error}
+ *   WCT has run all browsers, and is shutting down.
+ *
+ * Generic Events:
+ *
+ *  * log:debug
+ *  * log:info
+ *  * log:warn
+ *  * log:error
+ *
+ * @param {!Object} options The configuration, as specified in ./config.js.
+ * @param {function(*)} done callback indicating error or success.
+ * @return {!events.EventEmitter}
+ */
 module.exports = function test(options, done) {
+  // All of our internal entry points already have defaults merged, but we also
+  // want to expose this as the public API to web-component-tester.
   options = _.merge(config.defaults(), options);
-  var emitter = new events.EventEmitter();
 
+  var emitter = new events.EventEmitter();
   new CliReporter(emitter, options.output, options);
 
   // Add plugin event listeners
   _.values(options.plugins).forEach(function(plugin) {
     if (plugin.listener) {
-        new plugin.listener(emitter, options.output, plugin);
+      new plugin.listener(emitter, options.output, plugin);
     }
   });
 
