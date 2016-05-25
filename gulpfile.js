0c2a344bfa658cd289863f51064146c80693d242
Peter Burns <rictic@google.com>
2016-05-24 21:50:59 -0700

    Add depcheck and tslint to the test gulp target

--- gulpfile.js
+++ gulpfile.js
@@ -7,27 +7,35 @@
  * Code distributed by Google as part of the polymer project is also
  * subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
  */
+'use strict';
+
+const depcheck = require('depcheck');
 const fs = require('fs');
 const gulp = require('gulp');
 const jshint = require('gulp-jshint');
-const path = require('path');
 const lazypipe = require('lazypipe');
 const mocha = require('gulp-mocha');
+const path = require('path');
 const rollup = require('rollup');
 const runSequence = require('run-sequence');
 const ts = require('gulp-typescript');
-const tslint = require("gulp-tslint");
+const tslint = require('gulp-tslint');
+const typings = require('gulp-typings');
 
 const tsProject = ts.createProject('tsconfig.json', {
   typescript: require('typescript')
 });
+
+gulp.task('init', () => gulp.src('./typings.json').pipe(typings()));
+
+gulp.task('lint', ['tslint', 'test:style', 'depcheck']);
 
 // Meta tasks
 
 gulp.task('default', ['test']);
 
 gulp.task('test', function(done) {
-  runSequence(['build:typescript', 'test:style'], 'test:unit', done);
+  runSequence(['build:typescript', 'lint'], 'test:unit', done);
 });
 gulp.task('test:all', function(done) {
   runSequence('test', 'test:integration', done);
@@ -88,3 +96,36 @@ const jshintFlow = lazypipe()
   .pipe(jshint)
   .pipe(jshint.reporter, 'jshint-stylish')
   .pipe(jshint.reporter, 'fail');
+
+const usedUnusually = new Set([
+  // Used in browser.js
+  'accessibility-developer-tools',
+  'mocha',
+  'test-fixture',
+
+  // Used in the wct binary
+  'resolve'
+]);
+gulp.task('depcheck', () => new Promise((resolve, reject) => {
+  depcheck(__dirname, {ignoreDirs: []}, (result) => {
+    const invalidFiles = Object.keys(result.invalidFiles) || [];
+    const invalidJsFiles = invalidFiles.filter((f) => f.endsWith('.js'));
+    if (invalidJsFiles.length > 0) {
+      console.log('Invalid files:', result.invalidFiles);
+      reject(new Error('Invalid files'));
+      return;
+    }
+
+    const unused = new Set(result.dependencies);
+    for (const falseUnused of usedUnusually) {
+      unused.remove(falseUnused);
+    }
+    if (unused.size > 0) {
+      console.log('Unused dependencies:', unused);
+      reject(new Error('Unused dependencies'));
+      return;
+    }
+
+    resolve();
+  });
+}));
