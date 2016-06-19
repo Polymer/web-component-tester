3f01b0161450693648ed0af54f3e254b8d1a8e76
Peter Burns <rictic@google.com>
2016-06-19 10:59:07 -0700

    Convert test/unit/config.ts to idiomatic typescript

--- test/unit/config.ts
+++ test/unit/config.ts
@@ -7,21 +7,20 @@
  * Code distributed by Google as part of the polymer project is also
  * subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
  */
-var chai = require('chai');
+import * as chai from 'chai';
 
-var expect = chai.expect;
-
-var config  = require('../../runner/config');
-var Context = require('../../runner/context');
+import * as config from '../../runner/config';
+import {Context} from '../../runner/context';
+const expect = chai.expect;
 
 describe('config', function() {
 
   describe('.merge', function() {
 
     it('avoids modifying the input', function() {
-      var one    = {foo: 1};
-      var two    = {foo: 2};
-      var merged = config.merge(one, two);
+      const one    = <any>{foo: 1};
+      const two    = <any>{foo: 2};
+      const merged = <any>config.merge(one, two);
 
       expect(one.foo).to.eq(1);
       expect(two.foo).to.eq(2);
@@ -30,10 +29,10 @@ describe('config', function() {
     });
 
     it('honors false as an explicit blacklisting', function() {
-      var merged = config.merge(
-        {plugins: {foo: {}}},
-        {plugins: {foo: false}},
-        {plugins: {foo: {}, bar: {}}}
+      const merged = config.merge(
+        <any>{plugins: {foo: {}}},
+        <any>{plugins: {foo: false}},
+        <any>{plugins: {foo: {}, bar: {}}}
       );
 
       expect(merged).to.deep.equal({plugins: {foo: false, bar: {}}});
@@ -46,30 +45,35 @@ describe('config', function() {
     describe('deprecated options', function() {
 
       it('expands local string browsers', function() {
-        var context = new Context({browsers: ['chrome']});
+        const context = new Context(<any>{browsers: ['chrome']});
         return config.expand(context).then(() => {
-          expect(context.options.plugins.local.browsers).to.have.members(['chrome']);
+          expect(context.options.plugins['local'].browsers)
+              .to.have.members(['chrome']);
         });
       });
 
       it('expands sauce string browsers', function() {
-        var context = new Context({browsers: ['linux/firefox']});
+        const context = new Context(<any>{browsers: ['linux/firefox']});
         return config.expand(context).then(() => {
-          expect(context.options.plugins.sauce.browsers).to.have.members(['linux/firefox']);
+          expect(context.options.plugins['sauce'].browsers)
+              .to.have.members(['linux/firefox']);
         });
       });
 
       it('expands local object browsers', function() {
-        var context = new Context({browsers: [{browserName: 'firefox'}]});
+        const context = new Context(<any>{browsers: [{browserName: 'firefox'}]});
         return config.expand(context).then(() => {
-          expect(context.options.plugins.local.browsers).to.deep.have.members([{browserName: 'firefox'}]);
+          expect(context.options.plugins['local'].browsers)
+              .to.deep['have'].members([{browserName: 'firefox'}]);
         });
       });
 
       it('expands sauce object browsers', function() {
-        var context = new Context({browsers: [{browserName: 'safari', platform: 'OS X'}]});
+        const context = new Context(<any>
+            {browsers: [{browserName: 'safari', platform: 'OS X'}]});
         return config.expand(context).then(() => {
-          expect(context.options.plugins.sauce.browsers).to.deep.have.members([{browserName: 'safari', platform: 'OS X'}]);
+          expect(context.options.plugins['sauce'].browsers)
+              .to.deep['have'].members([{browserName: 'safari', platform: 'OS X'}]);
         });
       });
     });
