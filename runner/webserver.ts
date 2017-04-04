5209b03c3b57ba682e90e0fc12ee159c60f4e967
Henk vd Brink <hendrik.van.den.brink@ing.nl>
2017-04-04 10:06:45 +0200

    (fix): WCT ignores hostname, fixes: #523

--- runner/webserver.ts
+++ runner/webserver.ts
@@ -202,8 +202,9 @@ Expected to find a ${mdFilenames.join(' or ')} at: ${pathToLocalWct}/
 
     options.webserver._servers = servers.map(s => {
       const port = s.server.address().port;
+      const hostname = s.server.address().address;
       return {
-        url: `http://localhost:${port}${pathToGeneratedIndex}`,
+        url: `http://${hostname}:${port}${pathToGeneratedIndex}`,
         variant: s.kind === 'mainline' ? '' : s.variantName
       };
     });
