c363b65d025e122a3ddf5edb2bca85a027ab5ec2
Brendan Baldwin <brendan@usergenic.com>
2017-09-08 15:12:02 -0700

    Added a comment

--- test/fixtures/integration/query-string/test/tests.js
+++ test/fixtures/integration/query-string/test/tests.js
@@ -1,5 +1,6 @@
 var url = document.currentScript ?
   new URL(document.currentScript.src) :
+  // TODO(usergenic): Add the currentScript polyfill or remove this test.
   new URL('https://example.com/?fizz=buzz');
 
 test('preserves query strings (' + url.search + ')', function () {
