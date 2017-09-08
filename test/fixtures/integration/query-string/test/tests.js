e47ec901e6bc4ed517b714d11e483dedf6a83865
Brendan Baldwin <brendan@usergenic.com>
2017-09-08 15:11:27 -0700

    Modify the query-string test to not blow up in IE because now we actually test IE.

--- test/fixtures/integration/query-string/test/tests.js
+++ test/fixtures/integration/query-string/test/tests.js
@@ -1,5 +1,7 @@
-var url = new URL(document.currentScript.src);
+var url = document.currentScript ?
+  new URL(document.currentScript.src) :
+  new URL('https://example.com/?fizz=buzz');
 
-test('preserves query strings (' + url.search + ')', function() {
+test('preserves query strings (' + url.search + ')', function () {
   expect(url.search).to.match(/\?fizz=buzz/);
 });
