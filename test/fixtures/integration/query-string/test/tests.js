ed85cda314c56c3101d27970403426340ce3d50f
Brendan Baldwin <brendan@usergenic.com>
2017-09-08 15:48:22 -0700

    Oh right, IE11 took away the new URL() feature because... well they took it away.

--- test/fixtures/integration/query-string/test/tests.js
+++ test/fixtures/integration/query-string/test/tests.js
@@ -1,7 +1,8 @@
-// TODO(usergenic): Figure out how to get document.currentScript
-// to not make IE11 to break and then switch to the line below.
+// TODO(usergenic): Figure out a reasonable solution to get URL() and
+// document.currentScript to not make IE11 to break and then switch to
+// the line below.
 // var url = new URL(document.currentScript.src);
-var url = new URL('https://example.com/?fizz=buzz');
+var url = { search: '?fizz=buzz' };  // <-
 
 test('preserves query strings (' + url.search + ')', function () {
   expect(url.search).to.match(/\?fizz=buzz/);
