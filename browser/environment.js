f321d5977d85a306e15b0f71cc893188bb24d64f
Peter Burns <rictic@google.com>
2017-10-09 20:53:52 -0700

    Rename environment

--- browser/environment.js
+++ browser/environment.js
@@ -10,73 +10,67 @@
 import * as config from './config.js';
 import * as reporters from './reporters.js';
 import * as util from './util.js';
-
 /**
  * Loads all environment scripts ...synchronously ...after us.
  */
 export function loadSync() {
-  util.debug('Loading environment scripts:');
-  var a11ySuite = !!window.__wctUseNpm ?
-    'wct-browser-legacy/a11ySuite.js' : 'web-component-tester/data/a11ySuite.js';
-  var scripts = config.get('environmentScripts');
-  var a11ySuiteWillBeLoaded = window.__generatedByWct || scripts.indexOf(a11ySuite) > -1;
-
-  // We can't inject a11ySuite when running the npm version because it is a
-  // module-based script that needs `<script type=module>` and compilation 
-  // for browsers without module support.
-  if (!a11ySuiteWillBeLoaded && !window.__wctUseNpm) {
-    // wct is running as a bower dependency, load a11ySuite from data/
-    scripts.push(a11ySuite);
-  }
-  scripts.forEach(function (path) {
-    var url = util.expandUrl(path, config.get('root'));
-    util.debug('Loading environment script:', url);
-    // Synchronous load.
-    document.write('<script src="' + encodeURI(url) + '"></script>'); // jshint ignore:line
-  });
-  util.debug('Environment scripts loaded');
-
-  var imports = config.get('environmentImports');
-  imports.forEach(function (path) {
-    var url = util.expandUrl(path, config.get('root'));
-    util.debug('Loading environment import:', url);
-    // Synchronous load.
-    document.write('<link rel="import" href="' + encodeURI(url) + '">'); // jshint ignore:line
-  });
-  util.debug('Environment imports loaded');
+    util.debug('Loading environment scripts:');
+    var a11ySuite = !!window.__wctUseNpm ?
+        'wct-browser-legacy/a11ySuite.js' : 'web-component-tester/data/a11ySuite.js';
+    var scripts = config.get('environmentScripts');
+    var a11ySuiteWillBeLoaded = window.__generatedByWct || scripts.indexOf(a11ySuite) > -1;
+    // We can't inject a11ySuite when running the npm version because it is a
+    // module-based script that needs `<script type=module>` and compilation 
+    // for browsers without module support.
+    if (!a11ySuiteWillBeLoaded && !window.__wctUseNpm) {
+        // wct is running as a bower dependency, load a11ySuite from data/
+        scripts.push(a11ySuite);
+    }
+    scripts.forEach(function (path) {
+        var url = util.expandUrl(path, config.get('root'));
+        util.debug('Loading environment script:', url);
+        // Synchronous load.
+        document.write('<script src="' + encodeURI(url) + '"></script>'); // jshint ignore:line
+    });
+    util.debug('Environment scripts loaded');
+    var imports = config.get('environmentImports');
+    imports.forEach(function (path) {
+        var url = util.expandUrl(path, config.get('root'));
+        util.debug('Loading environment import:', url);
+        // Synchronous load.
+        document.write('<link rel="import" href="' + encodeURI(url) + '">'); // jshint ignore:line
+    });
+    util.debug('Environment imports loaded');
 }
-
 /**
  * We have some hard dependencies on things that should be loaded via
  * `environmentScripts`, so we assert that they're present here; and do any
  * post-facto setup.
  */
 export function ensureDependenciesPresent() {
-  _ensureMocha();
-  _checkChai();
+    _ensureMocha();
+    _checkChai();
 }
-
 function _ensureMocha() {
-  var Mocha = window.Mocha;
-  if (!Mocha) {
-    throw new Error('WCT requires Mocha. Please ensure that it is present in WCT.environmentScripts, or that you load it before loading web-component-tester/browser.js');
-  }
-  reporters.injectMocha(Mocha);
-  // Magic loading of mocha's stylesheet
-  var mochaPrefix = util.scriptPrefix('mocha.js');
-  // only load mocha stylesheet for the test runner output
-  // Not the end of the world, if it doesn't load.
-  if (mochaPrefix && window.top === window.self) {
-    util.loadStyle(mochaPrefix + 'mocha.css');
-  }
+    var Mocha = window.Mocha;
+    if (!Mocha) {
+        throw new Error('WCT requires Mocha. Please ensure that it is present in WCT.environmentScripts, or that you load it before loading web-component-tester/browser.js');
+    }
+    reporters.injectMocha(Mocha);
+    // Magic loading of mocha's stylesheet
+    var mochaPrefix = util.scriptPrefix('mocha.js');
+    // only load mocha stylesheet for the test runner output
+    // Not the end of the world, if it doesn't load.
+    if (mochaPrefix && window.top === window.self) {
+        util.loadStyle(mochaPrefix + 'mocha.css');
+    }
 }
-
 function _checkChai() {
-  if (!window.chai) {
-    util.debug('Chai not present; not registering shorthands');
-    return;
-  }
-
-  window.assert = window.chai.assert;
-  window.expect = window.chai.expect;
+    if (!window.chai) {
+        util.debug('Chai not present; not registering shorthands');
+        return;
+    }
+    window.assert = window.chai.assert;
+    window.expect = window.chai.expect;
 }
+//# sourceMappingURL=environment.js.map
